a81b6a830049004b27a56b2ea874b18ebcea4337
Ionnerss <c_beslea@live.concordia.ca>
2025-09-22 21:11:44 -0400

    new file

---
+++ PersonalProjects/ToDoList.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ToDoList {
+    public static void main(String[] args) {
+		Scanner keyReader = new Scanner(System.in);
+		System.out.println("Welcome to the ToDo List App!");
+		System.out.print("Would you like to create a new task (1 = yes/ 2 = no)? ");
+		int choice = keyReader.nextInt();
+		
+		if (choice != 1 || choice != 2) {
+
+		}
+		
+				
+
+	}
+	
+	public static int ChoiceVerif(int choice) {
+		
+		
+		
+		return 0;
+	}
+}
