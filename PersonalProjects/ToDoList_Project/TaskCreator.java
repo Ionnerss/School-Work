b30874e1e833211c105fd2cd4ddb697447b6f09b
Ionnerss <c_beslea@live.concordia.ca>
2025-10-10 16:48:51 -0400

    more work on TaskCreator

--- PersonalProjects/ToDoList_Project/TaskCreator.java
+++ PersonalProjects/ToDoList_Project/TaskCreator.java
@@ -6,8 +6,37 @@ import java.nio.file.Path;
 public class TaskCreator {
 
     public void TaskProcessor(String[] info){
+
         File file = new File("TaskLibrary.json");
-        Path path = file.toPath();
+
+        /*
+         * This represents the key/value pair they talked about in the vid. Basically the key (data)
+         * allows us to go fetch specific information about an object within this array. We have multiple ids
+         * which contain specific information. Thats what we'll use to store, modify, and view data.
+         *  {
+         *      "data": [
+         *           {"id": 1, "name": "Item A"},
+         *           {"id": 2, "name": "Item B"}
+         *      ]
+         *   }
+         */
+        String jsonString = "{\"data\": [{\"id\": 1, \"Title\": \"Item A\", \"Date\": \"february 2nd\", \"Description\": \"Whatever\"}, {\"id\": 2, \"Title\": \"Item B\", \"Date\": \"february 3rd\", \"Description\": \"Whatever\"}]}";
+
+        JSONObject jsonObject = new JSONObject(jsonString);
+        
+        JSONArray dataArray = jsonObject.getJSONArray("data");
+
+        JSONObject newItem = new JSONObject();
+        newItem.put("id", (dataArray.length() + 1));
+        newItem.put("Title", name);
+        newItem.put("Date", time);
+        newItem.put("Description", description);
+
+        dataArray.put(newItem);
+        jsonObject.put("data", dataArray);
+
+        String updatedjsonString = jsonObject.toString(2);
+        System.out.println(updatedjsonString);
 
 
 
