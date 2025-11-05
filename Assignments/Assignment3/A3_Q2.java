899c4f14f018514bcbe3dc573a11029ee83d9fe7
Ionnerss <c_beslea@live.concordia.ca>
2025-11-05 02:15:26 -0500

    fuckass a3q2 making my pc act up, a3q2 is done only comments left to do

--- Assignments/Assignment3/A3_Q2.java
+++ Assignments/Assignment3/A3_Q2.java
@@ -8,83 +8,54 @@ package Assignments.Assignment3;
 import java.util.Scanner;
 
 public class A3_Q2 {
-    static String species1;
-    static String species2;
-    static String species3;
-    static String species4;
-    static String species5;
-    static String[] species = {species1, species2, species3, species4, species5};
-    static int[] s1Zones;
-    static int[] s2Zones;
-    static int[] s3Zones;
-    static int[] s4Zones;
-    static int[] s5Zones;
-    static int[][] sZones = {s1Zones, s2Zones, s3Zones, s4Zones, s5Zones};
-    static int s1_Total;
-    static int s2_Total;
-    static int s3_Total;
-    static int s4_Total;
-    static int s5_Total;
-    static int[] totals = {s1_Total, s2_Total, s3_Total, s4_Total, s5_Total};
     static String selectedSpecies;
-    static int[] ssZone;
 
+    static String[] species, zones = {"Zone 1", "Zone 2", "Zone 3"};
 
+    static int[] selectedSZone, speciesTotals, zoneTotals;
 
-    static boolean mainMenu = true;
+    static int[][] sZones;
+
+    static double[] sAvgs, zoneAvgs;
+
+    static boolean mainMenuStart = true;
+
     static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to the Wildlife Sighting Matrix System");
         System.out.println("----------------------------------------------");
         System.out.println("Enter sightings for 5 species (format: name zone1 zone2 zone3)");
 
-        for (int i = 1 ; i <= 5 ; i++) {
-            System.out.print("Species " + i + ": ");
+        sZones = new int[5][3];
+        species = new String[5];
+        for (int i = 0 ; i <= 4 ; i++) {
+            System.out.print("Species " + (i + 1) + ": ");
             String input = scanner.next();
+            int num1 = scanner.nextInt();
+            int num2 = scanner.nextInt();
+            int num3 = scanner.nextInt();
+            scanner.nextLine(); // consume the remaining newline
 
-            switch (i) {
-                case 1:
-                    species1 = input.substring(0, input.indexOf(""));
-                    s1Zones = new int[] {input.indexOf(""), input.length()};
-                    break;
-                case 2:
-                    species2 = input.substring(0, input.indexOf(""));
-                    s2Zones = new int[] {input.indexOf(""), input.length()};
-                    break;
-                case 3:
-                    species3 = input.substring(0, input.indexOf(""));
-                    s3Zones = new int[] {input.indexOf(""), input.length()};
-                    break;
-                case 4:
-                    species4 = input.substring(0, input.indexOf(""));
-                    s4Zones = new int[] {input.indexOf(""), input.length()};
-                    break;
-                case 5:
-                    species5 = input.substring(0, input.indexOf(""));
-                    s5Zones = new int[] {input.indexOf(""), input.length()};
-                    break;
-            }
+            species[i] = input;
+            sZones[i] = new int[] {num1, num2, num3};
         }
-
-        for (int i = 0 ; i <=3 ; i++) {
-            s1_Total += s1Zones[i];
-            s2_Total += s2Zones[i];
-            s3_Total += s3Zones[i];
-            s4_Total += s4Zones[i];
-            s5_Total += s5Zones[i];
-        }
+        
+        totalsCalculator();
 
         System.out.println();
         System.out.println("Data recorded successfully!");
+        System.out.println();
 
         do {
-            mainMenu = MainMenu(mainMenu);
+            mainMenuStart = mainMenu(mainMenuStart);
         }
-        while(mainMenu);
+        while(mainMenuStart);
+        scanner.close();
     }
 
-    public static boolean MainMenu(boolean mainMenu) {
+    public static boolean mainMenu(boolean mainMenuStart) {
         System.out.println("=============== MENU ====================================");
         System.out.println("1. Display the sightings table");
         System.out.println("2. Update the sightings of a species in a specific zone");
@@ -95,112 +66,202 @@ public class A3_Q2 {
         System.out.println("7. Filter species by average sightings");
         System.out.println("8. Exit");
         System.out.println("=========================================================");
-        System.out.println("Enter your choice: ");
+        System.out.print("Enter your choice: ");
 
         int choice = scanner.nextInt();
+
         switch (choice) {
-            case 1:
-                System.out.println("Species     Zone1   Zone2   Zone3   Total");
+            case 1: 
+                System.out.printf("%-10s  %-5s   %-5s   %-5s   %-5s%n", "Species", "Zone1", "Zone2", "Zone3", "Total");
                 System.out.println("------------------------------------------");
-                System.out.println(species1 + "     " + s1Zones[0] + "  " + s1Zones[1] + "  " + s1Zones[2] + s1_Total);
-                System.out.println(species2 + "     " + s2Zones[0] + "  " + s2Zones[1] + "  " + s2Zones[2] + s2_Total);
-                System.out.println(species3 + "     " + s3Zones[0] + "  " + s3Zones[1] + "  " + s3Zones[2] + s3_Total);
-                System.out.println(species4 + "     " + s4Zones[0] + "  " + s4Zones[1] + "  " + s4Zones[2] + s4_Total);
-                System.out.println(species5 + "     " + s5Zones[0] + "  " + s5Zones[1] + "  " + s5Zones[2] + s5_Total);
+                System.out.printf("%-10s  %-5s   %-5s   %-5s   %-5s%n", species[0], sZones[0][0], sZones[0][1], sZones[0][2], speciesTotals[0]);
+                System.out.printf("%-10s  %-5s   %-5s   %-5s   %-5s%n", species[1], sZones[1][0], sZones[1][1], sZones[1][2], speciesTotals[1]);
+                System.out.printf("%-10s  %-5s   %-5s   %-5s   %-5s%n", species[2], sZones[2][0], sZones[2][1], sZones[2][2], speciesTotals[2]);
+                System.out.printf("%-10s  %-5s   %-5s   %-5s   %-5s%n", species[3], sZones[3][0], sZones[3][1], sZones[3][2], speciesTotals[3]);
+                System.out.printf("%-10s  %-5s   %-5s   %-5s   %-5s%n", species[4], sZones[4][0], sZones[4][1], sZones[4][2], speciesTotals[4]);
                 System.out.println("------------------------------------------");
                 break;
-            case 2:
-                System.out.println("Enter species name to update: ");
+            case 2: 
+                System.out.print("Enter species name to update: ");
                 selectedSpecies = scanner.next();
 
-                boolean exist = true;
+                boolean exist = false;
                 
                 for (int i = 0 ; i <= 4 ; i++) {
-                    if (selectedSpecies.equalsIgnoreCase(species[i])){
-                        selectedSpecies = species[i];
-                        ssZone = sZones[i];
-                        exist = true;
-                    }     
-                    else 
-                        exist = false;
+                    if (!selectedSpecies.equalsIgnoreCase(species[i]))
+                        continue;
+
+                    selectedSpecies = species[i];
+                    selectedSZone = sZones[i];
+                    exist = true;
                 }
 
                 if (exist == false)
                     System.out.println("Species not found.");
                 else {
-                    System.out.println("Enter zone number (1-3): ");
+                    System.out.print("Enter zone number (1-3): ");
                     int selectedZone = scanner.nextInt();
 
                     if ((selectedZone > 0) && (selectedZone < 4)) {
-                        System.out.println("Enter new sightings count: ");
+                        System.out.print("Enter new sightings count: ");
                         int newSightings = scanner.nextInt();
 
                         if (newSightings > 0) {
-                            ssZone[selectedZone] = newSightings;
+                            selectedSZone[selectedZone - 1] = newSightings;
+                            totalsCalculator();
+
                             System.out.println("Sightings updated successfully!");
                         }
                         else {
+                            selectedSZone[selectedZone - 1] = 0;
+                            totalsCalculator();
+
                             System.out.println("Sightings cannot be negative. Setting to 0.");
                             System.out.println("Sightings updated successfully!");
                         }
                     }
-                    else {
-                        System.out.println("Invalid zone number !");
-                    }
+                    else
+                        System.out.println("Invalid zone number!");
                 }
-                
+                System.out.println("------------------------------------------");
                 break;
-            case 3:
-                System.out.println("Enter species name to search: ");
+            case 3: 
+                System.out.print("Enter species name to search: ");
                 selectedSpecies = scanner.next();
                 int ssTotal = 0;
 
+                boolean speciesExists = false;
                 for (int i = 0 ; i <= 4 ; i++) {
-                    if (selectedSpecies.equalsIgnoreCase(species[i])){
-                        selectedSpecies = species[i];
-                        ssZone = sZones[i];
-                        ssTotal = totals[i];
-                    }     
-                    else 
+                    if (!selectedSpecies.equalsIgnoreCase(species[i]))
                         continue;
+
+                    speciesExists = true;
+                    selectedSpecies = species[i];
+                    selectedSZone = sZones[i];
+                    ssTotal = speciesTotals[i];
                 }
-                System.out.println(selectedSpecies + " -> " + "Zone 1: " + ssZone[0] + ", Zone 2: " + ssZone[1] + ", Zone 3: " + 
-                                    ssZone[2] + "(Total:" + ssTotal + ")");
+
+                if (speciesExists == false)
+                    System.out.println("Species not found.");
+                else
+                    System.out.println(selectedSpecies + " -> " + "Zone 1: " + selectedSZone[0] + ", Zone 2: " + selectedSZone[1] + ", Zone 3: " + 
+                                        selectedSZone[2] + " (Total: " + ssTotal + ")");
+                                    
+                System.out.println("------------------------------------------");
                 break;
             case 4:
                 int largestNum = 0;
                 
                 for (int i = 0 ; i <= 4 ; i++) {
-                    if (largestNum < totals[i]) {
-                        largestNum = totals[i];
-                        selectedSpecies = species[i];
-                    } 
-                    else
+                    if (largestNum > speciesTotals[i])
                         continue;
+                    if (largestNum == speciesTotals[i]) {
+                        largestNum = speciesTotals[i - 1];
+                        selectedSpecies = species[i - 1];
+                    }
+                    
+                    largestNum = speciesTotals[i];
+                    selectedSpecies = species[i];                
                 }    
                 System.out.println("Species with highest total sightings: " + selectedSpecies + " (" + largestNum + " sightings)");
+                System.out.println("------------------------------------------");
                 break;
             case 5:
-                int zone1Total = 0;
-                int zone2Total = 0;
-                int zone3Total = 0;
+                int lowestZone = (zoneTotals[0] + zoneTotals[1] + zoneTotals[2]); //random int which will strictly be bigger than any of the zone totals.
+                String specificZone = "";
 
-                for (int i = 0 ; i <= 3 ; i++) {
-                    zone1Total += sZones[i][0];
-                    zone2Total += sZones[i][1];
-                    zone3Total += sZones[i][2];
+                for (int i = 0; i <= 2 ; i++) {
+                    if (lowestZone < zoneTotals[i])
+                        continue;
+                    if (lowestZone == zoneTotals[i]) {
+                        lowestZone = zoneTotals[i - 1];
+                        specificZone = zones[i - 1];
+                    }
+
+                    lowestZone = zoneTotals[i];
+                    specificZone = zones[i];   
                 }
-
-                
+                System.out.println(specificZone + " has the lowest total sightings: " + lowestZone);
+                System.out.println("------------------------------------------");
                 break;
-            case 6:
+            case 6: //Works fine
+                System.out.println("Compute averages:");
+                System.out.println("1. Per species");
+                System.out.println("2. Per zone");
+
+                System.out.print("Enter choice: ");
+                choice = scanner.nextInt();
+
+                if (choice == 1) {
+                    for (int i = 0; i <= 4; i++) {
+                        System.out.print(species[i]);
+                        System.out.printf(" -> Average: %.2f%n", sAvgs[i]);
+                    }
+                }
+                else if (choice == 2) {
+                    for (int i = 0; i <= 2; i++) {
+                        System.out.print(zones[i]);
+                        System.out.printf(" -> Average: %.2f%n", zoneAvgs[i]);
+                    }
+                }
+                else
+                    System.out.println("Invalid choice!");
+
+                System.out.println("------------------------------------------");
                 break;
             case 7:
+                System.out.print("Enter threshold value: ");
+                double threshold = scanner.nextDouble();
+
+                System.out.println("Species with average >= " + threshold);
+
+                boolean thresholdIsSmaller = false;
+                for (int i = 0; i <= 4; i++) {
+                    if (!(sAvgs[i] >= threshold)) {
+                        continue;
+                    }
+
+                    thresholdIsSmaller = true;
+                    System.out.print("- " + species[i] + " (");
+                    System.out.printf("%.2f)%n", sAvgs[i]);
+                }
+
+                if (thresholdIsSmaller == false)
+                    System.out.println("No species meet the threshold.");
+
+                System.out.println("------------------------------------------");
                 break;
             case 8:
+                System.out.println("Thank you for supporting wildlife conservation. Goodbye!");
+                System.out.println("------------------------------------------");
+                mainMenuStart = false;
+                break;
+            default:
+                System.out.println("Invalid choice!");
                 break;
         }
+        return mainMenuStart;
+    }
+
+    public static void totalsCalculator() {
+        speciesTotals = new int[5];
+        zoneTotals = new int[3];
+        zoneAvgs = new double[3];
+        sAvgs = new double[5];
         
-        return mainMenu;
+        for (int i = 0; i <= 4; i++) {
+            for (int j = 0; j <= 2; j++) {
+                speciesTotals[i] += sZones[i][j];
+                sAvgs[i] += (sZones[i][j] / 3.0);
+            }
+        }
+
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 4; j++)
+                zoneTotals[i] += sZones[j][i];      
+        }
+
+        for (int i = 0; i <= 2; i++)
+            zoneAvgs[i] = zoneTotals[i] / 5.0;
     }
 } 
