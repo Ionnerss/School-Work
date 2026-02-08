91f5c8d0b19d77807b013c4398f7c815b49486f7
Ionnerss <c_beslea@live.concordia.ca>
2026-02-08 02:59:47 -0500

    s

--- AssignmentsS2/Assignment1/src/driver/Main.java
+++ AssignmentsS2/Assignment1/src/driver/Main.java
@@ -1,6 +1,12 @@
 package AssignmentsS2.Assignment1.src.driver;
 
+import java.util.Scanner;
+
 public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+
+
+    
     public static void main(String[] args) {
         
     }
