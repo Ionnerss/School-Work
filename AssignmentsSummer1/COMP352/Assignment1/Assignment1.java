25b5e8990148e1c9198e24b35fb6c07698098ad0
Catalin-Ion Besleaga <c_beslea@live.concordia.ca>
2026-05-13 14:54:01 -0400

    random

---
+++ AssignmentsSummer1/COMP352/Assignment1/Assignment1.java
@@ -0,0 +1,28 @@
+package AssignmentsSummer1.COMP352.Assignment1;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Assignment1 {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Welcome.");
+
+        System.out.print("Please enter number of columns: ");
+        int cols = sc.nextInt();
+        System.out.println();
+
+        System.out.print("Please enter number of rows: ");
+        int rows = sc.nextInt();
+        System.out.println();
+
+        System.out.print("Please enter percentage of obstacles" +
+                        " (the more obstacles the higher chance of no possible answers): ");
+        int percentage = sc.nextInt();
+
+
+
+        
+    }
+}
