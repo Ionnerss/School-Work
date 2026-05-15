5b9a89a580fa69ce5508d1d8e57c1ec921d5da98
Catalin-Ion Besleaga <c_beslea@live.concordia.ca>
2026-05-14 22:34:33 -0400

    wthelly dsa

--- AssignmentsSummer1/COMP352/Assignment1/Assignment1.java
+++ AssignmentsSummer1/COMP352/Assignment1/Assignment1.java
@@ -1,20 +1,60 @@
 package AssignmentsSummer1.COMP352.Assignment1;
 
 import java.util.Scanner;
-import java.util.LinkedList;
 import java.util.Random;
-
-/*
-- Implement a fixed base grid for testing the amount of paths feature and performance testing
-- fuck it 
-
-*/
-
 
 public class Assignment1 {
     static Scanner sc = new Scanner(System.in);
     static int[][] grid;
-    static int cols, rows, percentage, numPaths;
+    static int cols, rows, percentage, pathCount, shortestTurns, longestTurns;
+    static final int NONE = 0, RIGHT = 1, DOWN = 2;
+
+    static class State {
+        int row;
+        int col;
+        int previousDir;
+        int turnsSoFar;
+
+        State(int row, int col, int previousDir, int turnsSoFar) {
+            this.row = row;
+            this.col = col;
+            this.previousDir = previousDir;
+            this.turnsSoFar = turnsSoFar;
+        }
+    }
+
+    static class StateStack {
+        private State[] data;
+        private int top;
+
+        StateStack(int capacity) {
+            data = new State[capacity];
+            top = -1;
+        }
+
+        boolean isEmpty() {
+            return top == -1;
+        }
+
+        void push(State s) {
+            if (top == data.length - 1) {
+                System.out.println("Stack overflow");
+                return;
+            }
+
+            top++;
+            data[top] = s;
+        }
+
+        State pop() {
+            if (isEmpty())
+                return null;
+
+            State temp = data[top];
+            top--;
+            return temp;
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome.");
@@ -36,9 +76,29 @@ public class Assignment1 {
         generateGrid();
         displayGrid();
 
+        System.out.println();
+        System.out.print("Would you like to proceed with the recursive or iterative algorithm (1 = rec / 2 = it): ");
+        int choice = sc.nextInt();
+
+        shortestTurns = Integer.MAX_VALUE;
+        longestTurns = Integer.MIN_VALUE;
+        
+        if (choice == 1)
+            exploreRecursive(grid, 0, 0, NONE, 0);
+        else
+            exploreIterative(grid, 0, 0, NONE, 0);
+
+        System.out.println();
+        System.out.print("""
+            STATISTICS:
+            - Amount of possible paths found: %d
+            - Path with shortest amount of turns: %d
+            - Path with biggest amount of turns: %d
+
+        """.formatted(pathCount, shortestTurns, longestTurns));
     }
 
-    public static void generateGrid() {
+    private static void generateGrid() {
         Random random = new Random();
         
         grid[0][0] = 0;
@@ -57,9 +117,10 @@ public class Assignment1 {
                 grid[i][j] = 1;
             }
         }
+        System.out.println();
     } 
 
-    public static void displayGrid() {
+    private static void displayGrid() {
         System.out.println();
         String s = "-";
         for (int i = 0; i < cols; i++)
@@ -77,40 +138,66 @@ public class Assignment1 {
         }
     }
 
-    public static int validPathsRecursive() {
+    private static void exploreRecursive(int[][] grid, int row, int col, int previousDir, int turnsSoFar) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) return;
 
+        if (grid[row][col] == 1) return;
 
+        if (row == grid.length - 1 && col == grid[0].length - 1) {
+            pathCount++;
+            
+            if (turnsSoFar < shortestTurns) shortestTurns = turnsSoFar;
+            if (turnsSoFar > longestTurns) longestTurns = turnsSoFar;
 
+            return;
+        }
 
+        // 4. Try moving RIGHT
+        int turnsRight = updatedTurns(previousDir, RIGHT, turnsSoFar);
+        exploreRecursive(grid, row, col + 1, RIGHT, turnsRight);
 
-
-
-
-
-
-        return 0;
+        // 4. Try moving DOWN
+        int turnsDown = updatedTurns(previousDir, DOWN, turnsSoFar);
+        exploreRecursive(grid, row + 1, col, DOWN, turnsDown);
     }
 
-    public static int sumbs() {
-        
-        
-
-        
-
-
-
-
-
-
-
-
-        return 0;
+    private static int updatedTurns(int previousDirection, int newDirection, int turnsSoFar) {
+        if (previousDirection == NONE) return turnsSoFar;
+        else if (previousDirection == newDirection) return turnsSoFar;
+        else return ++turnsSoFar;
     }
 
+    private static void exploreIterative(int[][] grid, int row, int col, int previousDir, int turnsSoFar) {
+        StateStack stack = new StateStack(100000);
 
-    /*
-    if j = 1, check i, if i = 1, stop.
-    if j = 0, set new position, check j again, and so on
-    how tf do I make it not do the same path again
-    */
+        stack.push(new State(row, col, previousDir, turnsSoFar));
+        
+        while (!stack.isEmpty()) {
+            State current = stack.pop();
+
+            row = current.row;
+            col = current.col;
+            previousDir = current.previousDir;
+            turnsSoFar = current.turnsSoFar;
+
+            if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) continue;
+
+            if (grid[row][col] == 1) continue;
+
+            if (row == grid.length - 1 && col == grid[0].length - 1) {
+                pathCount++;
+                
+                if (turnsSoFar < shortestTurns) shortestTurns = turnsSoFar;
+                if (turnsSoFar > longestTurns) longestTurns = turnsSoFar;
+
+                continue;
+            }
+
+            int turnsRight = updatedTurns(previousDir, RIGHT, turnsSoFar);
+            stack.push(new State(row, col + 1, RIGHT, turnsRight));
+
+            int turnsDown = updatedTurns(previousDir, DOWN, turnsSoFar);
+            stack.push(new State(row + 1, col, DOWN, turnsDown));
+        }
+    }
 }
